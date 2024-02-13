e920b0f6454a6a95a40739432e181371c7b842bb
Saravana0699 <saravanatamilan06@gmail.com>
2024-02-13 11:51:47 +0530

    To check strings is Anagram

---
+++ Anagram.java
@@ -0,0 +1,37 @@
+public class Anagram {
+
+    public static void main(String[] args) {
+        String a = "incomplete";
+        String b = "completein";
+        boolean result = check(a, b);
+        if (result) {
+            System.out.println("The strings are anagrams. " + a + " And " + b);
+        } else {
+            System.out.println("The strings are not anagrams. " + a + " And " + b);
+        }
+
+    }
+
+    static boolean check(String a, String b) {
+        char[] tempa = new char[a.length()];
+        char[] tempb = new char[b.length()];
+
+        if (a.length() != b.length()) {
+            return false;
+        } else {
+            for (int i = 0; i < tempb.length; i++) {
+                tempa[i] = a.toLowerCase().charAt(i);
+                tempb[i] = b.toLowerCase().charAt(i);
+            }
+
+            java.util.Arrays.sort(tempa);
+            java.util.Arrays.sort(tempb);
+
+            for (int i = 0; i < tempb.length; i++) {
+                if (tempa[i] != tempb[i])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
